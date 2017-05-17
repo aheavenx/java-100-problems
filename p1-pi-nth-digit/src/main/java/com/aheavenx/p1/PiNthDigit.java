3aa95f6e7f62a11d3c186f0116ad0a52f9694e0a
Alex Skovorodnikov <Skovorodnikov@lanit.ru>
2017-05-17 13:05:40 +0300

    initial project structure

---
+++ p1-pi-nth-digit/src/main/java/com/aheavenx/p1/PiNthDigit.java
@@ -0,0 +1,11 @@
+package com.aheavenx.p1;
+
+/**
+ * @author Skovorodnikov
+ *         Created on 17.05.2017 13:05
+ */
+public class PiNthDigit {
+    public static void main(String[] args) {
+        
+    }
+}
