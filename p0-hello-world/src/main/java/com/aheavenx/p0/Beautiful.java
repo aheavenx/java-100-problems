6a7470dfbfe21a54b94552f6aba8435f7ce337b8
Alex Skovorodnikov <Skovorodnikov@lanit.ru>
2017-05-19 11:06:09 +0300

    more hello world obfuscation

--- p0-hello-world/src/main/java/com/aheavenx/p0/Beautiful.java
+++ p0-hello-world/src/main/java/com/aheavenx/p0/Beautiful.java
@@ -1,9 +1,12 @@
 package com.aheavenx.p0;
 
+import java.io.StringWriter;
 import java.math.BigInteger;
 import java.nio.file.AtomicMoveNotSupportedException;
+import java.util.Arrays;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -15,7 +18,16 @@ public class Beautiful {
     @Deprecated
     private void program() throws Throwable {
         final int _ = 0, __ = 1, ___ = 2, ____ = 3, _____ = 4, ______ = 5, _______ = 6, ________ = 7, _________ = 8, __________ = 9;
-        Class.forName("java.lang.System")
+        Class.forName(
+                Stream.of(
+                        ((Function<byte[], Byte[]>)($ -> { Byte[] $$ = new Byte[$.length]; Arrays.setAll($$, $$$ -> $[$$$]); return $$; }))
+                                .apply(new BigInteger(
+                                        Long.toString(0x6b6277622f6d626fL, 16) + Long.toString(0x682f547a7475666eL, 16), 16)
+                                        .toByteArray()))
+                        .map($ -> $ - 1)
+                        .map($ -> ((char) $.shortValue()))
+                        .collect(StringWriter::new, StringWriter::write, ($, $$) -> $.write($$.toString()))
+                        .toString())
                 .getDeclaredField(
                         new StringBuilder()
                                 .append(BiFunction.class.getName().charAt(__ << _____))
@@ -26,7 +38,16 @@ public class Beautiful {
                 .getType()
                 .getDeclaredMethod("print", Class.forName("java.lang.String"))
                 .invoke(
-                        Class.forName("java.lang.System").getDeclaredField("out").get(null),
+                        Class.forName(Stream.of(
+                                ((Function<byte[], Byte[]>)($ -> { Byte[] $$ = new Byte[$.length]; Arrays.setAll($$, $$$ -> $[$$$]); return $$; }))
+                                        .apply(new BigInteger(
+                                                Long.toString(0x6e657a6532706572L, 16) + Long.toString(0x6b32577d77786971L, 16), 16)
+                                                .toByteArray()))
+                                .map($ -> $ - 4)
+                                .map($ -> ((char) $.shortValue()))
+                                .collect(StringWriter::new, StringWriter::write, ($, $$) -> $.write($$.toString()))
+                                .toString())
+                                .getDeclaredField("out").get(null),
                         ((Function<BigInteger, ?>)(($$$) -> {
                             final BiFunction<BiFunction, BigInteger, ?> fnb = ($$, $) ->
                                     $.equals(BigInteger.ZERO) ? "" : new String(new char[]{(char) Integer.parseInt($.mod(BigInteger.valueOf(256)).toString())}) + $$.apply($$, $.divide(BigInteger.valueOf(256)));
@@ -42,8 +63,7 @@ public class Beautiful {
         try {
             System.out.println(AtomicMoveNotSupportedException.class.getSimpleName()); //print
             AtomicMoveNotSupportedException.class.getSimpleName().codePoints(); //todo make "print"
-
-            //new Beautiful().program();
+            new Beautiful().program();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
