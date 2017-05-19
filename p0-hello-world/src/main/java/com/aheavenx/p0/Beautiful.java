0f72198124a6d87ca5839b488598e558ff2b8b75
Alex Skovorodnikov <Skovorodnikov@lanit.ru>
2017-05-19 14:19:51 +0300

    Obfuscated beautifully

---
+++ p0-hello-world/src/main/java/com/aheavenx/p0/Beautiful.java
@@ -0,0 +1,34 @@
+package com.aheavenx.p0;
+import java.io.*; import java.math.*; import java.util.*; import java.util.function.*; import java.util.stream.*;
+public class Beautiful {
+    @Deprecated private void program() throws Throwable {
+
+        int _=0,__=1,___=2, _____=4,____=8;Class.forName(Stream.of(( (Function<byte[],Byte[] >)($->{Byte[]$$=new Byte[$.length];Arrays
+        .setAll($$,$$$->$[ $$$]);;;;return $$; })). apply (new BigInteger(Long .toString( 0x6b6277622f6d626fL,____<<__)+Long .toString
+        (0x682f547a7475666eL,_____<< ___),__<<_____).toByteArray())).map($->$- 1).map($->((char)$.shortValue( ))).collect(StringWriter
+
+
+        ::new,StringWriter::write,($,$$)->$.write($$.toString())).toString()).getDeclaredField(new StringBuilder().append(BiFunction//
+        .class.getName().charAt(__<<              _____)).append(Function.class.getName()            .charAt((__<<___)+__ ) ) .append(
+        getClass().getDeclaredMethods             ()[_].getAnnotations()[ _].toString( ).            toLowerCase( ) .charAt((__<<_____
+        )/**/+(__<<__))).toString()).             getType()./*?*/getDeclaredMethod(Stream            .concat(Stream.of( 82,80,75,76 ).
+        map($->$^34),Stream.of(0x74))             .map($->String.valueOf((char)$.intValue            ())).collect(Collectors.joining()
+        ),Class.forName(String.class.getName())).invoke(Class.forName(Stream.of(((Function<byte[],Byte[]>)( $->{Byte[]$$$=new Byte [$.
+        length];Arrays.setAll( $$$,$$             ->$[$$]);;;;;return $$$;})).apply( new             BigInteger(/*todo: try long*/Long.
+        toString(0x6e657a6532706572L,             _____<<___)+/*!TRY IT!*/Long.toString(             0x6b32577d77786971L ,__<<_____) ,
+        ____<<__).toByteArray())).map                                                                ($->$-4).map($->((char)$./*byte*/
+        shortValue( ) ) ) . collect (                                                                StringWriter::new,StringWriter ::
+        write,($,$$)->$.write(/**/$$.             toString()))./*.toString()*/toString())            .getDeclaredField(new String( new
+        byte[]{0x6f,0x75,0x74})).get(             null),((Function<BigInteger,?>)(($$$)->            {BiFunction<BiFunction,BigInteger
+        ,?>fnb=($$,$)->$.equals(BigInteger.ZERO)?"":new String(new char[]{(char)Integer.parseInt($.mod(BigInteger.valueOf(256))/*?*/ .
+        toString())})+$$.apply($$, $.             divide(BigInteger.valueOf(256)));return            fnb.apply(fnb, $$$);})).   apply(
+        BigInteger.valueOf(/*hexdec*/             8026160351752501L).multiply(BigInteger.            valueOf(100000000000000L ) ) .add
+        (BigInteger.valueOf(/*hex c*/             24568770929992L))));Class.forName(/*?*/            BinaryOperator.class.getName());;
+        for(int $=-178;;){$<<=$>>(_<<_____)>>_>>__;;$>>>=_<<__;break;}class klas{ private final transient int finishProgramNow = 42 ;}
+
+    }
+
+    public static void main(String[] args) throws Throwable {
+        new Beautiful().program();
+    }
+}
